1f97133295d6bb8760ec79c154c92ad077296b0a
Vladislav <vlad-vlasenko@mail.ru>
2022-01-12 16:16:32 +0300

    created email and pass

---
+++ controllers/userCtrl.js
@@ -0,0 +1,19 @@
+const Users = require('../models/userModel');
+
+const userCtrl = {
+  register: async (req, res) => {
+    try {
+        const {name, email, password} = req.bosy;
+
+        const user = await Users.findOne({email});
+        if(user) return res.status(400).json({msg: "The email is already exists."});
+
+        if(password.length < 6)
+          return res.status(400).json({msg: "Password is at least 6 characters long."});
+    } catch (err) {
+      return res.starus(500).json({msg: err.message});
+    }
+  }
+};
+
+module.exports = userCtrl;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
+const userCtrl = require('../controllers/userCtrl');
 
-router.post('/register', (req, res) => {
-  res.json({msg: "Test Router"});
-});
+router.post('/register', userCtrl.register);
 
 
 module.exports = router;
