21303fd7dd11bf20deec13a3977761b9d44fc416
Vladislav <vlad-vlasenko@mail.ru>
2022-01-13 17:20:53 +0300

    created refreshtoken and added router

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,42 +3,54 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const userCtrl = {
-  register: async (req, res) => {
-    try {
-      const {name, email, password} = req.body;
+    register: async (req, res) =>{
+        try {
+            const {name, email, password} = req.body;
 
-      const user = await Users.findOne({email});
-      if(user) return res.status(400).json({msg: "The email already exists."});
+            const user = await Users.findOne({email});
+            if(user) return res.status(400).json({msg: "The email already exists."});
 
-      if(password.length < 6)
-        return res.status(400).json({msg: "Password is at least 6 characters long."});
+            if(password.length < 6) 
+                return res.status(400).json({msg: "Password is at least 6 characters long."});
 
-      //Password Encryption
-      const passwordHash = await bcrypt.hash(password, 10);
-      const newUser = new Users({
-        name, email, password: passwordHash
-      });
+            // Password Encryption
+            const passwordHash = await bcrypt.hash(password, 10);
+            const newUser = new Users({
+                name, email, password: passwordHash
+            });
 
-      //Save mongodb
-      await newUser.save();
+            // Save mongodb
+            await newUser.save();
 
-      //Then create jsonwebtoken to authentication
-      const accesstoken = createAccessToken({id: newUser._id});
+            // Then create jsonwebtoken to authentication
+            const accesstoken = createAccessToken({id: newUser._id});
+            const refreshtoken = createRefreshToken({id: newUser._id});
 
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            });
 
-      res.json({accesstoken});
-      // res.json({msg: "Register Success!"});
+            res.json({accesstoken});
 
-
-    } catch (err) {
-      return res.status(500).json({msg: err.message});
+        } catch (err) {
+            return res.status(500).json({msg: err.message});
+        }
+    },
+    refreshToken: (req, res) =>{
+      
+      const rf_token = req.cookies.refreshtoken;  
+      
+      res.json({rf_token});
     }
-  }
+ };
+
+
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '11m'});
 };
-
-
-const createAccessToken = (user) => {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'});
 };
 
 module.exports = userCtrl;
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(URI, {
 });
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log('server is running on port', PORT);
 });
