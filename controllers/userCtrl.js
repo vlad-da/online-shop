5bd4c7bb157c3d0de56df04bd7d4e39ad6d937d1
Vladislav <vlad-vlasenko@mail.ru>
2022-01-14 12:37:59 +0300

    added authorization

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -88,6 +88,16 @@ const userCtrl = {
       const rf_token = req.cookies.refreshtoken;  
       if(!rf_token) 
       res.json({rf_token});
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('-password');
+            if(!user) return res.status(400).json({msg: "User does not exist."});
+
+            res.json(user);
+        } catch (err) {
+            return res.status(500).json({msg: err.message});
+        }
     }
  };
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userCtrl = require('../controllers/userCtrl');
+const auth = require('../middleware/auth');
 
 router.post('/register', userCtrl.register);
 
@@ -9,5 +10,6 @@ router.get('/logout', userCtrl.logout);
 
 router.get('/refresh_token', userCtrl.refreshToken);
 
+router.get('/infor', auth, userCtrl.getUser);
 
 module.exports = router;
