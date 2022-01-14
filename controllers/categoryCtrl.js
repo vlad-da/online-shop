71c321feaa670f72ca83766d6f1d5b60f9f0feb5
Vladislav <vlad-vlasenko@mail.ru>
2022-01-15 00:13:40 +0300

    added access admin to db

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -8,6 +8,23 @@ const categoryCtrl = {
       } catch (err) {
           return res.status(500).json({msg: err.message});
       }
+    },
+    createCategory: async (req, res) => {
+      try {
+          //if user have role = 1 ---> admin 
+          //Only admin can create, delete and update category
+          const {name} = req.body;
+          const categoty = await Category.findOne({name});
+          if(categoty) return res.status(400).json({msg: "This category already exists."});
+
+          const newCategory = new Category({name});
+
+          await newCategory.save();
+          res.json({msg: "Created a categoty"});
+          res.json('Check admin success');
+      } catch (err) {
+          return res.status(500).json({msg: err.message});
+      }
     }
 };
 
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const categoryCtrl = require('../controllers/categoryCtrl');
+const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAdmin');
 
 router.route('/category')
-    .get(categoryCtrl. getCategories);
-
+    .get(categoryCtrl. getCategories)
+    .post(auth, authAdmin, categoryCtrl.createCategory);
 module.exports = router;
