1fb72e2777435dbe9ac1f5f639c92784011c6a6c
Vladislav <vlad-vlasenko@mail.ru>
2022-01-17 22:19:58 +0300

    added get, create, delete, update of prducts

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(fileUpload({
 app.use('/user', require('./routes/userRouter'));
 app.use('/api', require('./routes/categoryRouter'));
 app.use('/api', require('./routes/upload'));
+app.use('/api', require('./routes/productRouter'));
 
 
 
