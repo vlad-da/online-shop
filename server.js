f19ad13ee817d6f4ca744e3ac305162772976c50
Vladislav <vlad-vlasenko@mail.ru>
2022-01-26 19:36:43 +0300

    added last chenges

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -15,7 +16,7 @@ app.use(
   })
 );
 
-//Routes
+// Routes
 app.use("/user", require("./routes/userRouter"));
 app.use("/api", require("./routes/categoryRouter"));
 app.use("/api", require("./routes/upload"));
@@ -35,6 +36,13 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
